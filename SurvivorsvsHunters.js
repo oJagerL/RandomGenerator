97335777c155fe6b89a701ae05b8d2c6e303e1e9
oJagerL <43181800+oJagerL@users.noreply.github.com>
2021-07-24 21:21:25 +0200

    Update SurvivorsvsHunters.js

--- SurvivorsvsHunters.js
+++ SurvivorsvsHunters.js
@@ -2,8 +2,6 @@ var NumberOfWords = 2
 
 var words = new BuildArray(NumberOfWords)
 
-// Use the following variables to
-// define your random words:
 words[1] = "Hunter"
 words[2] = "Survivor"
 
@@ -21,14 +19,4 @@ function PickRandomSpec(frm) {
 
 // Display the word inside the text box
     frm.WordBox.value = words[rnd]
-}
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
 }
