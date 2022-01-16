3fcc15bab34c320f7a5682a5a22370607727b7e6
oJagerL <43181800+oJagerL@users.noreply.github.com>
2022-01-16 16:38:25 +0100

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the date
-var countDownDate = new Date("October 16, 2021 19:45:00").getTime();
+var countDownDate = new Date("July 14, 2022 09:45:00").getTime();
 
 var x = setInterval(function () {
     var now = new Date().getTime();
